f288da3c27ab6e4d34f79052e3a3d60b7e2037cb
esther-malowa <malowaesther11@gmail.com>
2025-06-04 20:29:40 +0300

    Add untracked project files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+
+import React from 'react';
+import Landpage from './components/landpage';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Landpage />
+    </div>
+  );
+}
+
+export default App;
